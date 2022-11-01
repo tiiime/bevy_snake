f7a282651f622860ef7231f3e7d18a8cd3ec85f4
kang <kjxstar@gmail.com>
2022-11-01 19:01:22 +0800

    add movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::{
-    prelude::{default, App, Camera2dBundle, Color, Commands, Component, Transform, Vec3},
+    prelude::{
+        default, App, Camera2dBundle, Color, Commands, Component, Input, KeyCode, Query, Res,
+        Transform, Vec3,
+    },
     sprite::{Sprite, SpriteBundle},
     DefaultPlugins,
 };
@@ -14,6 +17,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
+        .add_system(snake_movement)
         .run()
 }
 
@@ -35,4 +39,24 @@ fn spawn_snake(mut commands: Commands) {
             ..default()
         })
         .insert(SnakeHead);
+}
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<(&SnakeHead, &mut Transform)>,
+) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 2.;
+        }
+    }
 }
