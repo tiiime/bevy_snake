b9db2fa84118b29ee054df3b28e6ca42f35aa98e
kang <kjxstar@gmail.com>
2022-11-01 17:13:16 +0800

    init camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::{
+    prelude::{App, Camera2dBundle, Commands},
+    DefaultPlugins,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run()
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
 }
