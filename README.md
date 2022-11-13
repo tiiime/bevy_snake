c279549cc4e495afbd9ead934e03d5a33c4da892
kang <kjxstar@gmail.com>
2022-11-13 15:26:33 +0800

    reinit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+## Snake
+
+position
+- {x,y}
+
+block 
+- {position}
+
+snake
+
+food
+
+eat_system
+- remove food && add block to tail(or dont remove tail)
+
+move_system
+- remove tail && add new head
+
+SnakeBlock entity
+{snake block}
+
+FoodBlock entity
+{block}
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,20 @@
-use bevy::{
-    prelude::{
-        default, App, Camera2dBundle, Color, Commands, Component, Input, KeyCode, Query, Res,
-        Transform, Vec3,
-    },
-    sprite::{Sprite, SpriteBundle},
-    DefaultPlugins,
-};
-
-#[derive(Component)]
-struct SnakeHead;
-
-const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+use iyes_loopless::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup_camera)
-        .add_startup_system(spawn_snake)
-        .add_system(snake_movement)
-        .run()
-}
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn_bundle(Camera2dBundle::default());
-}
-
-fn spawn_snake(mut commands: Commands) {
-    commands
-        .spawn_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: SNAKE_HEAD_COLOR,
-                ..default()
-            },
-            transform: Transform {
-                scale: Vec3::new(10.0, 10.0, 10.0),
-                ..default()
-            },
+        .insert_resource(WindowDescriptor {
+            width: 720.,
+            height: 720.,
             ..default()
         })
-        .insert(SnakeHead);
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(setup_world)
+        .run()
 }
-
-fn snake_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<(&SnakeHead, &mut Transform)>,
-) {
-    for (_head, mut transform) in head_positions.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 2.;
-        }
-        if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 2.;
-        }
-        if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 2.;
-        }
-        if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 2.;
-        }
-    }
+ 
+fn setup_world(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
 }
